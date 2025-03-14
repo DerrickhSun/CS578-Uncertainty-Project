e004df42ed6d7dcf1d4107dc02ef071033d3d73c
DerrickhSun <derrick.han.sun@gmail.com>
2025-03-14 14:37:37 -0400

    current progress

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -30,6 +30,7 @@ columns = ['id', 'item_id', 'dept_id', 'cat_id', 'store_id', 'state_id', 'd',
        'snap_CA', 'snap_TX', 'snap_WI', 'sell_price']
 categoricals = ["id", "item_id", "dept_id", "cat_id", "store_id", "state_id", "event_name_1", "event_type_1"]#, "event_name_2", "event_type_2"
 encodeables = ["dept_id", "cat_id", "state_id", "event_name_1", "event_type_1"]
+# drop weekday, dept_id?
 ints = ['sales', 'wm_yr_wk', 'wday', 'month', 'year', "snapCA", "snapTX", "snapWI", "sell_price"]
 other = ['d', 'date', 'weekday']
 drop = ["d", "date", "event_type_2", "event_name_2", "wm_yr_wk"]
@@ -51,14 +52,51 @@ def fill_nulls(df):
     return df
 
 def encode(df):
+    # encode categorical variables
     for en in encodeables:
-        print("cat:",en)
-        onehot = pd.get_dummies(df[en]).add_prefix(en+":")
-        print(onehot)
+        #print("cat:",en)
+        onehot = pd.get_dummies(df[en]).add_prefix(en+":").astype(int)
+        #print(onehot)
         df = df.join(onehot)
     df.drop(columns=encodeables,inplace=True)
+
+    # encode day to a number
+    df["d"] = df["d"].apply(lambda x: int(x[2:]))
     return df
 
+
+def add_cols(df):
+    # adding last couple days data
+    right = df[["item_id", "store_id", "d", "sales"]].rename(columns={"d":"last"})
+    last = "d"
+
+    remove = []
+    for day in range(28):
+        df["last"] = df[last].apply(lambda x: x - 1)
+        last = "last"
+
+        df = df.merge(right, how="left", on=["item_id","store_id","last"], suffixes=("","_"+str(day)))
+        remove.append("sales_"+str(day))
+    
+    df["item_store_last_day_sales"] = df["sales_0"]
+    df.to_csv("test.csv")
+    df["item_store_last_day_sales_filled"] = df["sales_0"].isnull().astype(int) # mark null days
+    df["item_store_last_day_sales"] = df["item_store_last_day_sales"].fillna(value=df["item_store_last_day_sales"].mean(skipna=True)) # fill null days with mean
+    
+    for med in [7, 14, 21, 28]:
+        cols = ["sales_"+str(i) for i in range(med)]
+        #print(df[cols].median())
+        df["item_store_L"+str(med)+"d_day_median_sales"] = df[cols].median(skipna=False)
+        df["item_store_L"+str(med)+"d_day_median_sales_filled"] = df[cols].median(skipna=False).isnull().astype(int)
+        #print(df.head())
+
+    df.drop(columns = remove,inplace=True)
+    #print(df.isnull().any())
+    #print(df.head())
+
+    return df
+
+        
 
 
 
@@ -74,6 +112,11 @@ print(df.head())
 df = fill_nulls(df).head()
 print(df)
 print(df.dtypes)
-print(encode(df).dtypes)
+df = encode(df)
+print(df.dtypes)
+df = add_cols(df)
+print(df.dtypes)
+print(df.isnull().any())
+print(df.notnull().any())
 #print(df['sell_price'].isnull().sum())
 #print(df.groupby(["d","sell_price"]).count())
